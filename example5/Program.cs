e8230833e5a566a9c56688ce62745815bb26ab7c
Artr <example.com>
2022-10-13 22:31:18 +0300

    new method

--- example5/Program.cs
+++ example5/Program.cs
@@ -1,8 +1,14 @@
-﻿Console.WriteLine("Введите число N");
+﻿//Console.WriteLine("Введите число N");
+//string input = Console.ReadLine();
+//if (input.Length == 3) {
+//   System.Console.WriteLine(input[input.Length-1]); 
+//}
+//else {
+//   System.Console.WriteLine("Введите трехзначное число");
+//}
+
+Console.WriteLine("Введите число N");
 string input = Console.ReadLine();
-if (input.Length == 3) {
-   System.Console.WriteLine(input[input.Length-1]); 
-}
-else {
-    System.Console.WriteLine("Введите трехзначное число");
-}
+int A = int.Parse(input);
+int kkk = A%10;
+Console.WriteLine(kkk);
