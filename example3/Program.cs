b68432aa86bb1104c67441015c9eb6c30e5df60b
Artr <example.com>
2022-10-13 21:48:27 +0300

    example3

---
+++ example3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите номер дня недели");
+string Sa = Console.ReadLine();
+int a = int.Parse(Sa);
+switch (a)
+{
+    case 1: Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+
+}
