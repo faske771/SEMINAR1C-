3c4dcd2536edad3b8945953aceb53e260a16df7e
Artr <example.com>
2022-10-13 22:22:51 +0300

    ex4-5

---
+++ example4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N");
+string N = Console.ReadLine();
+int Number = int.Parse(N);
+int Number2 = Number * (-1);
+while (Number2<Number)
+{
+    Console.Write(Number2);
+    Number2++;
+}
---
+++ example5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число N");
+string input = Console.ReadLine();
+if (input.Length == 3) {
+   System.Console.WriteLine(input[input.Length-1]); 
+}
+else {
+    System.Console.WriteLine("Введите трехзначное число");
+}
