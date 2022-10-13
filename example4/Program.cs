609f9fe46dd1be98d6b678df9f9d94642b1b4401
Artr <example.com>
2022-10-13 22:36:58 +0300

    fix example4

--- example4/Program.cs
+++ example4/Program.cs
@@ -2,7 +2,7 @@
 string N = Console.ReadLine();
 int Number = int.Parse(N);
 int Number2 = Number * (-1);
-while (Number2<Number)
+while (Number2<=Number)
 {
     Console.Write(Number2);
     Number2++;
