0d2a663fb00d7a8c917b9738d9e8b95fa853e429
Artr <example.com>
2022-10-13 21:36:39 +0300

    example2

---
+++ example2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число A");
+string А = Console.ReadLine();
+int numberA = int.Parse(А);
+Console.WriteLine("Введите число В");
+string B = Console.ReadLine();
+int numberB = int.Parse(B);;
+if (numberB == numberA * numberA) Console.WriteLine("Да");
+else 
+{
+if (numberA == numberB * numberB) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+}
+
+
