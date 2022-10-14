b874b02e53ce0a69a3df36b52058dfff2d4cab22
Artr <example.com>
2022-10-14 21:33:41 +0300

    ex9&10

---
+++ example10/Program.cs
@@ -0,0 +1,19 @@
+﻿int number1 = int.Parse(Console.ReadLine());
+if (number1 % 7 == 0 && number1 % 23 == 0)
+{
+Console.WriteLine(number1 + " кратно 7 и 23");
+}
+else 
+{
+Console.WriteLine(number1 + " НЕ кратно 7 и 23");
+}
+
+//int A = int.Parse(Console.ReadLine());
+//if (A % 7 ==0 && A % 23 =00)
+//{
+//    System.Console.WriteLine(A + " кратно 7 и 23 ");
+//}
+//else 
+//{
+//    System.Console.WriteLine(A + " не кратно 7 и 23 ");
+//}
---
+++ example9/Program.cs
@@ -0,0 +1,22 @@
+﻿string input1 = Console.ReadLine();
+int A = int.Parse(input1);
+string input2 = Console.ReadLine();
+int B = int.Parse(input2);
+int ost = 0;
+if (A%B==0)
+{
+    System.Console.WriteLine("true");
+}
+else
+{
+    ost = A%B;
+    if (B%A==0)
+    {
+        System.Console.WriteLine("true");
+    }
+    else 
+    {
+        ost = B%A;  
+        System.Console.WriteLine(ost);
+    }
+}
