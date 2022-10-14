961f51d705831d53f355aa1c502941c7b9c0a47b
Artr <example.com>
2022-10-14 20:48:28 +0300

    ex8

---
+++ example8/Program.cs
@@ -0,0 +1,9 @@
+﻿Random rnd = new Random();
+int number = (int)rnd.Next(100,999);
+int first = 0;
+int second = 0;
+first = number / 100;
+second = number % 10;
+System.Console.WriteLine(number);
+System.Console.Write(first);
+System.Console.Write(second);
