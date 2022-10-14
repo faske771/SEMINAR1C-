b5a6861afb0d505f17adef3c397673111c9495e3
Artr <example.com>
2022-10-14 21:53:11 +0300

    ex11

---
+++ example11/Program.cs
@@ -0,0 +1,17 @@
+﻿int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+if (A*A == B)
+{
+    System.Console.WriteLine("true");
+}
+else
+{
+    if (B*B == A)
+    {
+        System.Console.WriteLine("true");
+    }
+    else
+    {
+    System.Console.WriteLine("false");
+    }
+}
