c1beddd8f42dfbb32a5c9cbae023fc586b449cad
Artr <example.com>
2022-10-14 20:35:50 +0300

    ex7

---
+++ example7/Program.cs
@@ -0,0 +1,12 @@
+﻿Random rnd = new Random();
+int number = (int)rnd.Next(10,99);
+int first = 0;
+int second = 0;
+first = number / 10;
+second = number % 10;
+if (first>second){
+    System.Console.WriteLine(first);
+}
+else {
+    System.Console.WriteLine(second);
+}
