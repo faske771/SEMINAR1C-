aaa0c8b9c38b7185623f5b343ffdac81a6b54627
Artr <example.com>
2022-10-14 20:24:06 +0300

    ex1s1hw

---
+++ example6-homework/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число A");
+string A = Console.ReadLine();
+int Ac = int.Parse(A);
+Console.WriteLine("Введите число B");
+string B = Console.ReadLine();
+int Bc = int.Parse(B);
+int max = 0;
+int min = 0;
+if (Ac == Bc)
+{
+    Console.WriteLine("A = B");
+}
+else
+{
+    if (Ac > Bc)
+    {
+        max = Ac;
+        min = Bc;
+    }
+    else
+    {
+        max = Bc;
+        min = Ac;
+    }
+    Console.Write("max = ");
+    Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.WriteLine(min);
+}
